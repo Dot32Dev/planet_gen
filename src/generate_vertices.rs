937572670373889be6276ab1dc25270a38e174d4
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2023-05-29 17:22:13 +0800

    Initial commit

---
+++ src/generate_vertices.rs
@@ -0,0 +1,409 @@
+use noise::{NoiseFn, Simplex, Perlin};
+
+pub fn generate_vertices(
+	square_size: f32, // Detail level
+	z_value: f32,
+	lerped: bool,
+	x_y_scale: f32,
+	darkness: f32,
+) -> (std::vec::Vec<[f32; 3]>, std::vec::Vec<[f32; 3]>, std::vec::Vec<[f32; 2]>, std::vec::Vec<u32>) {
+	let simplex = Simplex::new(1);
+
+    let rows = 600/square_size as usize;
+    let cols = 600/square_size as usize;
+
+    // Create a 2D vector of squares
+    let mut grid = vec![vec![0.0; cols + 1]; rows + 1];
+
+    for row in 0..=rows {
+        for col in 0..=cols {
+            let x = col as f32 * square_size - (cols as f32 * square_size) / 2.;
+            let y = row as f32 * square_size - (rows as f32 * square_size) / 2.;
+
+            let noise = simplex.get([x as f64 * x_y_scale as f64, y as f64 * x_y_scale as f64, z_value as f64]) as f32 + darkness;
+
+            grid[row][col] = noise;
+
+            // commands.spawn(MaterialMesh2dBundle {
+            //     mesh: meshes.add(shape::Circle::new(square_size*0.1).into()).into(),
+            //     material: materials.add(ColorMaterial::from(Color::rgb(noise, noise, noise))),
+            //     transform: Transform::from_translation(Vec3::new(x, y, 0.1)),
+            //     ..default()
+            // });
+        }
+    }
+
+    let mut positions: Vec<[f32; 3]> = Vec::new();
+    let mut normals: Vec<[f32; 3]> = Vec::new();
+    let mut uvs: Vec<[f32; 2]> = Vec::new();
+    let mut indices: Vec<u32> = Vec::new();
+
+    // Iterate over 4 grid points at a time
+    for row in 0..rows {
+        for col in 0..cols {
+            // let value = (grid[row][col]*8.0 + grid[row][col + 1]*4.0 + grid[row + 1][col + 1]*2.0 + grid[row + 1][col]*1.0) as u8;
+            // generate_tile(
+            //     grid: &Vec<Vec<f32>>,
+            //     row: usize,
+            //     col: usize,
+            //     square_size: f32,
+            //     rows: usize,
+            //     cols: usize,
+            //     positions: &mut Vec<[f32; 3]>,
+            //     normals: &mut Vec<[f32; 3]>,
+            //     uvs: &mut Vec<[f32; 2]>,
+            //     indices: &mut Vec<u32>,	
+            //     detail_level: usize,
+            //     z_value: f32,
+            //     lerped: bool,
+            // )
+            let value = ((grid[row][col]+0.5).floor()*8.0 + (grid[row][col + 1]+0.5).floor()*4.0 + (grid[row + 1][col + 1]+0.5).floor()*2.0 + (grid[row + 1][col]+0.5).floor()*1.0) as u8;
+	
+			let left_x = col as f32 * square_size - (cols as f32 * square_size) / 2.;
+			let top_y = row as f32 * square_size - (rows as f32 * square_size) / 2.;
+
+			let right_x = left_x + square_size;
+			let bottom_y = top_y + square_size;
+
+			let mut top = inverse_lerp(grid[row][col], grid[row][col + 1], 0.5);
+			let mut right = inverse_lerp(grid[row][col + 1], grid[row + 1][col + 1], 0.5);
+			let mut bottom = inverse_lerp(grid[row + 1][col], grid[row + 1][col + 1], 0.5);
+			let mut left = inverse_lerp(grid[row][col], grid[row + 1][col], 0.5);
+
+			if !lerped {
+				// If not lerped, the top, right, bottom, and left values are set to 0.5
+				top = 0.5;
+				right = 0.5;
+				bottom = 0.5;
+				left = 0.5;
+			}
+
+			match value {
+				0 => {}
+				1 => {
+					// Top left corner
+					positions.push([left_x, top_y + square_size*left, 0.0]);
+					positions.push([left_x, bottom_y, 0.0]);
+					positions.push([left_x + square_size*bottom, bottom_y, 0.0]);
+
+					for _ in 0..3 {
+						normals.push([0.0, 0.0, 1.0]);
+						uvs.push([0.0, 0.0]);
+					}
+
+					indices.push((positions.len() - 3) as u32);
+					indices.push((positions.len() - 2) as u32);
+					indices.push((positions.len() - 1) as u32);
+				}
+				2 => {
+					// Top right corner
+					positions.push([left_x + square_size*bottom, bottom_y, 0.0]);
+					positions.push([right_x, top_y + square_size*right, 0.0]);
+					positions.push([right_x, bottom_y, 0.0]);
+
+					for _ in 0..3 {
+						normals.push([0.0, 0.0, 1.0]);
+						uvs.push([0.0, 0.0]);
+					}
+
+					indices.push((positions.len() - 3) as u32);
+					indices.push((positions.len() - 2) as u32);
+					indices.push((positions.len() - 1) as u32);
+				}
+				3 => {
+					// Top rectangle
+					positions.push([left_x, top_y + square_size*left, 0.0]);
+					positions.push([right_x, top_y + square_size*right, 0.0]);
+					positions.push([right_x, bottom_y, 0.0]);
+					positions.push([left_x, bottom_y, 0.0]);
+
+					for _ in 0..4 {
+						normals.push([0.0, 0.0, 1.0]);
+						uvs.push([0.0, 0.0]);
+					}
+
+					indices.push((positions.len() - 4) as u32);
+					indices.push((positions.len() - 3) as u32);
+					indices.push((positions.len() - 2) as u32);
+					indices.push((positions.len() - 4) as u32);
+					indices.push((positions.len() - 2) as u32);
+					indices.push((positions.len() - 1) as u32);
+				}
+				4 => {
+					// Bottom right corner
+					positions.push([right_x, top_y, 0.0]);
+					positions.push([right_x, top_y + square_size*right, 0.0]);
+					positions.push([left_x + square_size*top, top_y, 0.0]);
+
+					for _ in 0..3 {
+						normals.push([0.0, 0.0, 1.0]);
+						uvs.push([0.0, 0.0]);
+					}
+
+					indices.push((positions.len() - 3) as u32);
+					indices.push((positions.len() - 2) as u32);
+					indices.push((positions.len() - 1) as u32);
+				}
+				5 => {
+					// Top left AND bottom right corners
+					// Top left corner
+					positions.push([left_x, top_y + square_size*left, 0.0]);
+					positions.push([left_x, bottom_y, 0.0]);
+					positions.push([left_x + square_size*bottom, bottom_y, 0.0]);
+					// Bottom right corner
+					positions.push([right_x, top_y, 0.0]);
+					positions.push([right_x, top_y + square_size*right, 0.0]);
+					positions.push([left_x + square_size*top, top_y, 0.0]);
+
+					for _ in 0..6 {
+						normals.push([0.0, 0.0, 1.0]);
+						uvs.push([0.0, 0.0]);
+					}
+
+					// Top left corner
+					indices.push((positions.len() - 6) as u32);
+					indices.push((positions.len() - 5) as u32);
+					indices.push((positions.len() - 4) as u32);
+					// Bottom right corner
+					indices.push((positions.len() - 3) as u32);
+					indices.push((positions.len() - 2) as u32);
+					indices.push((positions.len() - 1) as u32);
+				}
+				6 => {
+					// Right rectangle
+					positions.push([left_x + square_size*top, top_y, 0.0]);
+					positions.push([right_x, top_y, 0.0]);
+					positions.push([right_x, bottom_y, 0.0]);
+					positions.push([left_x + square_size*bottom, bottom_y, 0.0]);
+
+					for _ in 0..4 {
+						normals.push([0.0, 0.0, 1.0]);
+						uvs.push([0.0, 0.0]);
+					}
+
+					indices.push((positions.len() - 4) as u32);
+					indices.push((positions.len() - 3) as u32);
+					indices.push((positions.len() - 2) as u32);
+					indices.push((positions.len() - 4) as u32);
+					indices.push((positions.len() - 2) as u32);
+					indices.push((positions.len() - 1) as u32);
+				}
+				7 => {
+					// The opposite of the bottom left corner, made from 3 triangles
+					positions.push([left_x, bottom_y, 0.0]); // Top left
+					positions.push([right_x, bottom_y, 0.0]); // Top right
+					positions.push([right_x, top_y, 0.0]); // Bottom right
+					positions.push([left_x + square_size*top, top_y, 0.0]); // Bottom center
+					positions.push([left_x, top_y + square_size*left, 0.0]); // Center left
+
+					for _ in 0..5 {
+						normals.push([0.0, 0.0, 1.0]);
+						uvs.push([0.0, 0.0]);
+					}
+
+					// Triangle 1
+					indices.push((positions.len() - 5) as u32);
+					indices.push((positions.len() - 4) as u32);
+					indices.push((positions.len() - 3) as u32);
+					// Triangle 2
+					indices.push((positions.len() - 5) as u32);
+					indices.push((positions.len() - 3) as u32);
+					indices.push((positions.len() - 2) as u32);
+					// Triangle 3
+					indices.push((positions.len() - 5) as u32);
+					indices.push((positions.len() - 2) as u32);
+					indices.push((positions.len() - 1) as u32);
+				}
+				8 => {
+					// Bottom left corner
+					positions.push([left_x, top_y, 0.0]);
+					positions.push([left_x + square_size*top, top_y, 0.0]);
+					positions.push([left_x, top_y + square_size*left, 0.0]);
+
+					for _ in 0..3 {
+						normals.push([0.0, 0.0, 1.0]);
+						uvs.push([0.0, 0.0]);
+					}
+
+					indices.push((positions.len() - 3) as u32);
+					indices.push((positions.len() - 2) as u32);
+					indices.push((positions.len() - 1) as u32);
+				}
+				9 => {
+					// Left rectangle
+					positions.push([left_x, top_y, 0.0]);
+					positions.push([left_x + square_size*top, top_y, 0.0]);
+					positions.push([left_x + square_size*bottom, bottom_y, 0.0]);
+					positions.push([left_x, bottom_y, 0.0]);
+
+					for _ in 0..4 {
+						normals.push([0.0, 0.0, 1.0]);
+						uvs.push([0.0, 0.0]);
+					}
+
+					indices.push((positions.len() - 4) as u32);
+					indices.push((positions.len() - 3) as u32);
+					indices.push((positions.len() - 2) as u32);
+					indices.push((positions.len() - 4) as u32);
+					indices.push((positions.len() - 2) as u32);
+					indices.push((positions.len() - 1) as u32);
+				}
+				10 => {
+					// Top right AND bottom left corners
+					// Top right corner
+					positions.push([left_x + square_size*bottom, bottom_y, 0.0]);
+					positions.push([right_x, top_y + square_size*right, 0.0]);
+					positions.push([right_x, bottom_y, 0.0]);
+					// Bottom left corner
+					positions.push([left_x, top_y, 0.0]);
+					positions.push([left_x + square_size*top, top_y, 0.0]);
+					positions.push([left_x, top_y + square_size*left, 0.0]);
+					
+					for _ in 0..6 {
+						normals.push([0.0, 0.0, 1.0]);
+						uvs.push([0.0, 0.0]);
+					}
+
+					// Top right corner
+					indices.push((positions.len() - 6) as u32);
+					indices.push((positions.len() - 5) as u32);
+					indices.push((positions.len() - 4) as u32);
+					// Bottom left corner
+					indices.push((positions.len() - 3) as u32);
+					indices.push((positions.len() - 2) as u32);
+					indices.push((positions.len() - 1) as u32);
+				}
+				11 => {
+					// The opposite of the bottom right corner, made from 3 triangles
+					positions.push([right_x, bottom_y, 0.0]); // Top right
+					positions.push([left_x, bottom_y, 0.0]); // Top left
+					positions.push([left_x, top_y, 0.0]); // Bottom left
+					positions.push([left_x + square_size*top, top_y, 0.0]); // Bottom center
+					positions.push([right_x, top_y + square_size*right, 0.0]); // Center right
+					
+
+					for _ in 0..5 {
+						normals.push([0.0, 0.0, 1.0]);
+						uvs.push([0.0, 0.0]);
+					}
+
+					// Triangle 1
+					indices.push((positions.len() - 5) as u32);
+					indices.push((positions.len() - 4) as u32);
+					indices.push((positions.len() - 3) as u32);
+					// Triangle 2
+					indices.push((positions.len() - 5) as u32);
+					indices.push((positions.len() - 3) as u32);
+					indices.push((positions.len() - 2) as u32);
+					// Triangle 3
+					indices.push((positions.len() - 5) as u32);
+					indices.push((positions.len() - 2) as u32);
+					indices.push((positions.len() - 1) as u32);                  
+				}
+				12 => {
+					// Bottom rectangle
+					positions.push([right_x, top_y + square_size*right, 0.0]); // right
+					positions.push([left_x, top_y + square_size*left, 0.0]); // left
+					positions.push([left_x, top_y, 0.0]);
+					positions.push([right_x, top_y, 0.0]);
+
+					for _ in 0..4 {
+						normals.push([0.0, 0.0, 1.0]);
+						uvs.push([0.0, 0.0]);
+					}
+
+					indices.push((positions.len() - 4) as u32);
+					indices.push((positions.len() - 3) as u32);
+					indices.push((positions.len() - 2) as u32);
+					indices.push((positions.len() - 4) as u32);
+					indices.push((positions.len() - 2) as u32);
+					indices.push((positions.len() - 1) as u32);
+				}
+				13 => {
+					// Opposite of the top left corner, made from 3 triangles
+					positions.push([right_x, top_y, 0.0]); // Bottom right
+					positions.push([left_x, top_y, 0.0]); // Bottom left
+					positions.push([left_x, bottom_y, 0.0]); // Top left
+					positions.push([left_x + square_size*bottom, bottom_y, 0.0]); // Top center
+					positions.push([right_x, top_y + square_size*right, 0.0]); // Center right
+
+					for _ in 0..5 {
+						normals.push([0.0, 0.0, 1.0]);
+						uvs.push([0.0, 0.0]);
+					}
+
+					// Triangle 1
+					indices.push((positions.len() - 5) as u32);
+					indices.push((positions.len() - 4) as u32);
+					indices.push((positions.len() - 3) as u32);
+					// Triangle 2
+					indices.push((positions.len() - 5) as u32);
+					indices.push((positions.len() - 3) as u32);
+					indices.push((positions.len() - 2) as u32);
+					// Triangle 3
+					indices.push((positions.len() - 5) as u32);
+					indices.push((positions.len() - 2) as u32);
+					indices.push((positions.len() - 1) as u32);
+				}
+				14 => {
+					// The opposite of the top left corner, made from 3 triangles
+					positions.push([left_x, top_y, 0.0]); // bottom left
+					positions.push([right_x, top_y, 0.0]); // bottom right
+					positions.push([right_x, bottom_y, 0.0]); // top right
+					positions.push([left_x + square_size*bottom, bottom_y, 0.0]); // top center
+					positions.push([left_x, top_y + square_size*left, 0.0]); // Center left
+
+					for _ in 0..5 {
+						normals.push([0.0, 0.0, 1.0]);
+						uvs.push([0.0, 0.0]);
+					}
+
+					// Triangle 1
+					indices.push((positions.len() - 5) as u32);
+					indices.push((positions.len() - 4) as u32);
+					indices.push((positions.len() - 3) as u32);
+					// Triangle 2
+					indices.push((positions.len() - 5) as u32);
+					indices.push((positions.len() - 3) as u32);
+					indices.push((positions.len() - 2) as u32);
+					// Triangle 3
+					indices.push((positions.len() - 5) as u32);
+					indices.push((positions.len() - 2) as u32);
+					indices.push((positions.len() - 1) as u32);
+				}
+				15 => {
+					//Square
+					positions.push([left_x, top_y, 0.0]);
+					positions.push([right_x, top_y, 0.0]);
+					positions.push([right_x, bottom_y, 0.0]);
+					positions.push([left_x, bottom_y, 0.0]);
+
+					for _ in 0..4 {
+						normals.push([0.0, 0.0, 1.0]);
+						uvs.push([0.0, 0.0]);
+					}
+
+					// Triangle1
+					indices.push((positions.len() - 4) as u32);
+					indices.push((positions.len() - 3) as u32);
+					indices.push((positions.len() - 2) as u32);
+					// Triangle2
+					indices.push((positions.len() - 4) as u32);
+					indices.push((positions.len() - 2) as u32);
+					indices.push((positions.len() - 1) as u32);
+				}
+				_ => {}
+			};
+        }
+    }
+
+	return (positions, normals, uvs, indices);
+}
+
+fn inverse_lerp(a: f32, b: f32, value: f32) -> f32 {
+    if a == b {
+        return 0.0;
+    }
+
+    return (value - a) / (b - a);
+}
---
+++ src/main.rs
@@ -0,0 +1,203 @@
+use bevy::ui;
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use bevy::render::mesh::{self, PrimitiveTopology, Indices};
+use bevy::sprite::Mesh2dHandle;
+use bevy_egui::{egui, EguiContexts, EguiPlugin};
+use bevy_prototype_debug_lines::*;
+
+mod generate_vertices;
+use generate_vertices::generate_vertices;
+
+#[derive(Default, Resource)]
+struct UiState {
+    detail_level: u32,
+    z_value: f32,
+    lerped: bool,
+    animate: bool,
+    x_y_scale: f32,
+    wireframe: bool,
+    darkness: f32,
+}
+
+#[derive(Component)]
+struct MarchingSquares {
+    size: f32,
+}
+
+fn main() {
+    App::new()
+        // .init_resource::<UiState>()
+        .insert_resource(UiState {
+            detail_level: 10,
+            z_value: 0.0,
+            lerped: true,
+            animate: false,
+            x_y_scale: 0.007,
+            wireframe: false,
+            darkness: 0.5,
+        })
+        .insert_resource(ClearColor(Color::rgb(0.68, 0.97, 0.99)))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Marching Squares".into(),
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_plugin(EguiPlugin)
+        .add_plugin(DebugLinesPlugin::default())
+        .add_startup_system(setup)
+        .add_system(ui_example_system)
+        .add_system(marching_squares_system)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn((
+        MaterialMesh2dBundle {
+            transform: Transform::from_xyz(0.0, 0.0, 0.1),
+            material: materials.add(Color::rgb(108.0/255.0, 177.0/255.0, 5.0/255.0).into()),
+            ..default()
+        }, 
+        MarchingSquares {
+            size: 300.0,
+        },
+    ));
+    commands.spawn((
+        MaterialMesh2dBundle {
+            transform: Transform::from_xyz(0.0, 0.0, 0.2),
+            material: materials.add(Color::rgb(0.61, 0.86, 0.26).into()),
+            ..default()
+        }, 
+        MarchingSquares {
+            size: 295.0,
+        },
+    ));
+    commands.spawn((
+        MaterialMesh2dBundle {
+            transform: Transform::from_xyz(0.0, 0.0, 0.3),
+            material: materials.add(Color::rgb(0.53, 0.5, 0.43).into()),
+            ..default()
+        }, 
+        MarchingSquares {
+            size: 270.0,
+        },
+    ));
+}
+
+fn ui_example_system(
+    mut contexts: EguiContexts,
+    mut ui_state: ResMut<UiState>,
+    time: Res<Time>,
+) {
+    egui::Window::new("Settings").show(contexts.ctx_mut(), |ui| {
+        ui.label(format!("FPS: {}", (1.0 / Time::delta_seconds(&time)).floor()));
+        ui.add(egui::Slider::new(&mut ui_state.detail_level, 3..=50).text("Grid Size (Lower is better)"));
+        // ui.label("Z Value");
+        ui.add(egui::Slider::new(&mut ui_state.z_value, 0.0..=100.0).text("Z Coordinate"));
+        ui.add(egui::Slider::new(&mut ui_state.x_y_scale, 0.001..=0.04).text("X/Y Scale (Zoom)"));
+        ui.add(egui::Slider::new(&mut ui_state.darkness, 0.0..=1.0).text("Darkness"));
+        // ui.label("Lerped or midpoint");
+        ui.checkbox(&mut ui_state.lerped, "Lerped");
+        ui.checkbox(&mut ui_state.animate, "Animate");
+        ui.checkbox(&mut ui_state.wireframe, "Wireframe");
+    });
+
+    if ui_state.animate {
+        ui_state.z_value += 0.01;
+    }
+}
+
+fn marching_squares_system(
+    ui_state: Res<UiState>,
+    mut marching_squares_meshes: Query<(&mut Mesh2dHandle, &MarchingSquares)>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut lines: ResMut<DebugLines>,
+) {
+    // println!("marching_squares_meshes: {:?}", marching_squares_meshes.iter().len());
+    if ui_state.is_changed() {
+        for (mut mesh_handle, marching_square) in marching_squares_meshes.iter_mut() {
+            // let mut mesh = meshes.get_mut(mesh_handle);
+            // if mesh.is_some() {
+            //     let (positions, normals, uvs, indices) = generate_tiles(ui_state.detail_level as f32, ui_state.z_value as f32, ui_state.lerped);
+                
+            //     let mut new_mesh = Mesh::new(PrimitiveTopology::TriangleList);
+            //     new_mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, positions);
+            //     new_mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
+            //     new_mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
+            //     new_mesh.set_indices(Some(Indices::U32(indices)));
+
+            //     mesh = Some(&mut new_mesh);
+
+            //     println!("Updated mesh")
+            // }
+            let (mut positions, normals, uvs, mut indices) = generate_vertices(ui_state.detail_level as f32, ui_state.z_value as f32, ui_state.lerped, ui_state.x_y_scale, ui_state.darkness);
+                
+            // Remove triangles that are outside of the circle
+            // let mut to_remove = Vec::new();
+            for i in (0..indices.len() / 3).rev() {
+                if indices.len() >= i + 3 {
+                    let i = i * 3;
+                    let a = positions[indices[i] as usize];
+                    let b = positions[indices[i + 1] as usize];
+                    let c = positions[indices[i + 2] as usize];
+                    let dist_a = Vec3::new(a[0], a[1], 0.0).length();
+                    let dist_b = Vec3::new(b[0], b[1], 0.0).length();
+                    let dist_c = Vec3::new(c[0], c[1], 0.0).length();
+                    if dist_a > marching_square.size && dist_b > marching_square.size && dist_c > marching_square.size {
+                        // positions.remove(indices[i + 2] as usize);
+                        // positions.remove(indices[i + 1] as usize);
+                        // positions.remove(indices[i] as usize);
+                        indices.remove(i + 2);
+                        indices.remove(i + 1);
+                        indices.remove(i);
+                    }
+                }
+            }
+            // for i in to_remove.iter() {
+            //     indices.remove(to_remove[*i] as usize);
+            //     positions.remove(to_remove[*i] as usize);
+            // }
+            
+            // Clap vertices to a radius of 300
+            for i in 0..positions.len() {
+                let mut pos = Vec3::new(positions[i][0], positions[i][1], 0.0);
+                let dist = pos.length();
+                if dist > marching_square.size {
+                    pos = pos.normalize() * marching_square.size;
+                }
+                positions[i] = pos.into();
+            }
+            
+            if ui_state.wireframe {
+                // Draw debug lines for every triangle (fake wireframe)
+                for i in 0..indices.len() / 3 {
+                    let i = i * 3;
+                    let a = positions[indices[i] as usize];
+                    let b = positions[indices[i + 1] as usize];
+                    let c = positions[indices[i + 2] as usize];
+                    // lines.line_colored(a.extend(0.1), b.extend(0.1), 0.1, Color::RED);
+                    // lines.line_colored(b.extend(0.1), c.extend(0.1), 0.1, Color::RED);
+                    // lines.line_colored(c.extend(0.1), a.extend(0.1), 0.1, Color::RED);
+                    lines.line_colored(Vec3::new(a[0], a[1], 0.1), Vec3::new(b[0], b[1], 0.1), 0.0, Color::BLACK);
+                    lines.line_colored(Vec3::new(b[0], b[1], 0.1), Vec3::new(c[0], c[1], 0.1), 0.0, Color::BLACK);
+                    lines.line_colored(Vec3::new(c[0], c[1], 0.1), Vec3::new(a[0], a[1], 0.1), 0.0, Color::BLACK);
+                }
+            }
+
+            let mut new_mesh = Mesh::new(PrimitiveTopology::TriangleList);
+            new_mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, positions);
+            new_mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
+            new_mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
+            new_mesh.set_indices(Some(Indices::U32(indices)));
+
+            *mesh_handle = meshes.add(new_mesh).into();
+        }
+    }
+}
